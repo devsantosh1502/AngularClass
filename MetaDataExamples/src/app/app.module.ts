42e8c50482c0335c33b08241832cd6155da24707
santosh kumar <santosh.k.j2015@hotmail.com>
2019-10-28 22:31:06 +0530

    Input and output  component created

--- MetaDataExamples/src/app/app.module.ts
+++ MetaDataExamples/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InputoutputComponent } from './inputoutput/inputoutput.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputoutputComponent
   ],
   imports: [
     BrowserModule
